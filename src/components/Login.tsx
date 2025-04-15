4dd928aeed165cd6ac68b291160d20c7a2d1d6b8
JosMaria <josmariaguilar@gmail.com>
2025-04-14 20:44:54 -0400

    modifying login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,5 @@
 import styles from './Login.module.scss';
+import logoUrl from '../assets/logo.svg';
 
 export const Login = () => {
 
@@ -6,16 +7,20 @@ export const Login = () => {
         <dialog className={styles.dialogContent} open={true}>
             <div className={styles.position}>
                 <section className={styles.modalContainer}>
+                    <img src={logoUrl} alt='Logo' width={80} />
+                    
                     <h1>Inicio de Sesión</h1>
-                    <div className={styles.inputContainer}>
-                        <label htmlFor="username">Usuario</label>
-                        <input id="username" type="text" />
-                    </div>
-                    <div className={styles.inputContainer}>
-                        <label htmlFor="username">Contrase&#241;a</label>
-                        <input id="username" type="text" />
-                    </div>
-                    <button className={styles.button} type="submit">Iniciar Sesión</button>
+                    <form className={styles.formContainer}>
+                        <div className={styles.inputContainer}>
+                            <label htmlFor="username">Usuario</label>
+                            <input id="username" type="text" />
+                        </div>
+                        <div className={styles.inputContainer}>
+                            <label htmlFor="username">Contrase&#241;a</label>
+                            <input id="username" type="text" />
+                        </div>
+                        <button className={styles.button} type="submit">Iniciar Sesión</button>
+                    </form>
                 </section>
             </div>
         </dialog>
