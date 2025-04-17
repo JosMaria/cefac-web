3fcff33620760001f656b9ebdde847e5b21f1714
JosMaria <josmariaguilar@gmail.com>
2025-04-16 20:33:27 -0400

    adding global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import styles from './App.module.scss';
-import { LoginPage } from "./pages/Login/LoginPage.tsx";
+import { LoginPage } from './pages/Login/LoginPage.tsx';
 
 function App() {
 
     return (
         <main className={styles.mainContent}>
-            <LoginPage/>
+            <LoginPage />
         </main>
 
     )
---
+++ src/pages/Account/AccountPage.tsx
@@ -0,0 +1,13 @@
+export const AccountPage = () => {
+  return (
+    
+    <section>
+      <h1>JosMaria</h1>
+      <label>Nombre</label>
+      <input type="text" />
+
+    </section>
+
+
+  );
+}
