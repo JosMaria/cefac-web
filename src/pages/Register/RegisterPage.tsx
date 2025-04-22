0847a40ba8951424d4b635b270d1499cf1dd7caf
JosMaria <josmariaguilar@gmail.com>
2025-04-22 12:58:19 -0400

    creating messages

--- src/pages/Register/RegisterPage.tsx
+++ src/pages/Register/RegisterPage.tsx
@@ -52,9 +52,6 @@ export const RegisterPage = () => {
         <h1>Registro de Usuario</h1>
         <section className={styles.msgSection}>
           <MessageSection />
-          <div className={styles.msgContainer}>
-            Este tutorial fue diseñado para personas que prefieren aprender haciendo y quieren ver algo tangible de manera rápida. Si prefieres aprender cada concepto paso a paso, comienza con Describir la UI.
-          </div>
         </section>
 
 
@@ -104,36 +101,100 @@ const MessageSection = () => {
   const changeValue = (newValue: MessageType) => setValue(prev => prev !== newValue ? newValue : 'none');
 
   return (
-    <div className={styles.iconContainer}>
-      <svg
-        className={`${styles.icon} ${styles.information} ${value === 'information' ? styles.selected : ''} `}
-        onClick={() => changeValue('information')}
-        width='35' height='35' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'
-      >
-        <circle cx='12' cy='12' r='10' fill='#1E88E5' />
-        <path d='M12 16V12' stroke='white' strokeWidth='2' strokeLinecap='round' />
-        <circle cx='12' cy='8' r='1' fill='white' />
-      </svg>
-      <svg
-        className={`${styles.icon} ${styles.warning} ${value === 'warning' ? styles.selected : ''}`}
-        onClick={() => changeValue('warning')}
-        width='35' height='35' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'
-      >
-        <path d='M10.308 4L2.492 18C2.221 18.482 2.387 19.091 2.869 19.362C3.016 19.441 3.182 19.483 3.351 19.483H20.649C21.202 19.483 21.649 19.036 21.649 18.483C21.649 18.314 21.607 18.148 21.528 18.001L13.692 4C13.421 3.518 12.812 3.352 12.33 3.623C12.117 3.742 11.942 3.917 11.823 4.13L10.308 4Z'
-          fill='#FFC107' />
-        <path d='M12 16V12' stroke='#000000' strokeWidth='1.5' strokeLinecap='round' />
-        <circle cx='12' cy='9' r='1' fill='#000000' />
-      </svg>
-      <svg
-        className={`${styles.icon} ${styles.danger} ${value === 'danger' ? styles.selected : ''}`}
-        onClick={() => changeValue('danger')}
-        width='35' height='35' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'
-      >
-        <rect x='5.757' y='5.757' width='12.485' height='12.485' rx='1' transform='rotate(45 12 12)' fill='#FF3B30' stroke='#D70015' stroke-width='1.5' />
-        <path d='M12 8V12' stroke='white' stroke-width='2' stroke-linecap='round' />
-        <circle cx='12' cy='15' r='1' fill='white' />
-      </svg>
-    </div>
+    <section className={styles.messageContainer}>
+      <div className={styles.iconContainer}>
+        <svg
+          className={`${styles.icon} ${styles.information} ${value === 'information' ? styles.selected : ''}`}
+          onClick={() => changeValue('information')}
+          width='35' height='35' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'
+        >
+          <circle cx='12' cy='12' r='10' fill='#1E88E5' />
+          <path d='M12 16V12' stroke='white' strokeWidth='2' strokeLinecap='round' />
+          <circle cx='12' cy='8' r='1' fill='white' />
+        </svg>
+        <svg
+          className={`${styles.icon} ${styles.warning} ${value === 'warning' ? styles.selected : ''}`}
+          onClick={() => changeValue('warning')}
+          width='35' height='35' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'
+        >
+          <path d='M10.308 4L2.492 18C2.221 18.482 2.387 19.091 2.869 19.362C3.016 19.441 3.182 19.483 3.351 19.483H20.649C21.202 19.483 21.649 19.036 21.649 18.483C21.649 18.314 21.607 18.148 21.528 18.001L13.692 4C13.421 3.518 12.812 3.352 12.33 3.623C12.117 3.742 11.942 3.917 11.823 4.13L10.308 4Z'
+            fill='#FFC107' />
+          <path d='M12 16V12' stroke='#000000' strokeWidth='1.5' strokeLinecap='round' />
+          <circle cx='12' cy='9' r='1' fill='#000000' />
+        </svg>
+        <svg
+          className={`${styles.icon} ${styles.danger} ${value === 'danger' ? styles.selected : ''}`}
+          onClick={() => changeValue('danger')}
+          width='35' height='35' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'
+        >
+          <rect x='5.757' y='5.757' width='12.485' height='12.485' rx='1' transform='rotate(45 12 12)' fill='#FF3B30' stroke='#D70015' stroke-width='1.5' />
+          <path d='M12 8V12' stroke='white' stroke-width='2' stroke-linecap='round' />
+          <circle cx='12' cy='15' r='1' fill='white' />
+        </svg>
+      </div>
+      <div className={styles.informationContainer}>
+        <header className={styles.headerContainer}>
+          <div className={styles.title}>
+            <svg width='22' height='22' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+              <circle cx='12' cy='12' r='10' fill='#1E88E5' />
+              <path d='M12 16V12' stroke='white' strokeWidth='2' strokeLinecap='round' />
+              <circle cx='12' cy='8' r='1' fill='white' />
+            </svg>
+            <h3>Informaci&oacute;n</h3>
+          </div>
+          <svg width='20' height='20' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+            <path d='M18 6L6 18' stroke='#808080' strokeWidth='2' strokeLinecap='round' />
+            <path d='M6 6L18 18' stroke='#808080' strokeWidth='2' strokeLinecap='round' />
+          </svg>
+        </header>
+
+        <div className={styles.messageContent}>
+          Este tutorial fue diseñado para personas que prefieren aprender haciendo y quieren ver algo tangible de manera rápida. Si prefieres aprender cada concepto paso a paso, comienza con Describir la UI.
+        </div>
+      </div>
+      <div className={styles.warningContainer}>
+        <header className={styles.headerContainer}>
+          <div className={styles.title}>
+            <svg width='22' height='22' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+              <path fill='#FFC107' d='M10.308 4L2.492 18C2.221 18.482 2.387 19.091 2.869 19.362C3.016 19.441 3.182 19.483 3.351 19.483H20.649C21.202 19.483 21.649 19.036 21.649 18.483C21.649 18.314 21.607 18.148 21.528 18.001L13.692 4C13.421 3.518 12.812 3.352 12.33 3.623C12.117 3.742 11.942 3.917 11.823 4.13L10.308 4Z' />
+              <path d='M12 16V12' stroke='#000000' strokeWidth='1.5' strokeLinecap='round' />
+              <circle cx='12' cy='9' r='1' fill='#000000' />
+            </svg>
+            <h3>Consideraciones</h3>
+          </div>
+          <svg width='20' height='20' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+            <path d='M18 6L6 18' stroke='#808080' strokeWidth='2' strokeLinecap='round' />
+            <path d='M6 6L18 18' stroke='#808080' strokeWidth='2' strokeLinecap='round' />
+          </svg>
+        </header>
+        <div className={styles.messageContent}>
+          <ul>
+            <li>El nombre de usuario y el correo electronico no deber&iacute;an haber sido registrados anteriormente.</li>
+            <li>El alias tambien podria considerarse como rol que tiene el usuario dentro del sistema, por preferencia una palabra que lo defina claramente.</li>
+            <li>El n&uacute;mero de celular se tomara en cuenta como la contraseña al momento del registro.</li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.dangerContainer}>
+        <header className={styles.headerContainer}>
+          <div className={styles.title}>
+            <svg width='22' height='22' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+              <rect x='5.757' y='5.757' width='12.485' height='12.485' rx='1' transform='rotate(45 12 12)' fill='#FF3B30' stroke='#D70015' stroke-width='1.5' />
+              <path d='M12 8V12' stroke='white' stroke-width='2' stroke-linecap='round' />
+              <circle cx='12' cy='15' r='1' fill='white' />
+            </svg>
+            <h3>Riesgos</h3>
+          </div>
+          <svg width='20' height='20' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+            <path d='M18 6L6 18' stroke='#808080' strokeWidth='2' strokeLinecap='round' />
+            <path d='M6 6L18 18' stroke='#808080' strokeWidth='2' strokeLinecap='round' />
+          </svg>
+        </header>
+        <div className={styles.messageContent}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, excepturi deserunt id adipisci molestiae, rem illo expedita tenetur itaque quam tempora fugiat dolor quidem temporibus, eum sequi ullam doloribus ipsa?
+        </div>
+      </div>
+    </section>
   );
 }
 
@@ -154,11 +215,7 @@ const Warning = () => {
           <path d='M6 6L18 18' stroke='#808080' strokeWidth='2' strokeLinecap='round' />
         </svg>
       </header>
-      <ul>
-        <li>El nombre de usuario y el correo electronico no deber&iacute;an haber sido registrados anteriormente.</li>
-        <li>El alias tambien podria considerarse como rol que tiene el usuario dentro del sistema, por preferencia una palabra que lo defina claramente.</li>
-        <li>El n&uacute;mero de celular se tomara en cuenta como la contraseña al momento del registro.</li>
-      </ul>
+
     </article>
   )
 }
