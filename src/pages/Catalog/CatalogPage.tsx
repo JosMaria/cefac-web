866904d3d7f0c38637bdb8fc20d250128d63e5e7
JosMaria <josmariaguilar@gmail.com>
2025-06-02 23:57:34 -0400

    creating product list

--- src/pages/Catalog/CatalogPage.tsx
+++ src/pages/Catalog/CatalogPage.tsx
@@ -17,6 +17,13 @@ const PRODUCTS: CatalogType[] = [
     tag: 'Crema Antiarrugas',
     price: 40,
     imageUrl: 'https://media.gettyimages.com/id/173616090/es/foto/frascos-de-cosm%C3%A9ticos.jpg?s=612x612&w=0&k=20&c=fJ3YbFzLhef_WwEfF2MQG67LSvkeKow9poe3PZglVb4='
+  },
+  {
+    id: '2',
+    name: 'BIOCLEAN',
+    tag: 'Lavajillas',
+    price: 15,
+    imageUrl: 'https://cdn.create.vista.com/api/media/small/209552570/stock-photo-close-view-arranged-skin-care-creams-fruits-palm-leaf-beige'
   }
 ];
 
@@ -24,24 +31,27 @@ export const CatalogPage = () => {
   return (
     <div className={styles.container}>
       {PRODUCTS.length === 0 ?
-          <section className={styles.section}>
-            <p className={styles.message}>
-              Por el momento, no tenemos productos disponibles para mostrar,
-              pero estamos trabajando para traerte novedades muy pronto.
-              Gracias por sus compresi&oacute;n
-            </p>
-            <br />
-            <Link className={styles.link} to='..'>&larr; Volver</Link>
-          </section>
-          :
-          PRODUCTS.map(product => 
-            <div className={styles.cardContainer} key={product.name}>
-              <img src={product.imageUrl} alt={product.name} width={300} />
-              <h4>{product.name}</h4>
+        <section className={styles.section}>
+          <p className={styles.message}>
+            Por el momento, no tenemos productos disponibles para mostrar,
+            pero estamos trabajando para traerte novedades muy pronto.
+            Gracias por sus compresi&oacute;n
+          </p>
+          <br />
+          <Link className={styles.link} to='..'>&larr; Volver</Link>
+        </section>
+        :
+        <section className={styles.productList}>
+          {PRODUCTS.map(product =>
+            <Link className={styles.cardContainer} to='login' key={product.name}>
+              <img className={styles.image} src={product.imageUrl} alt={product.name} width={300} />
               <h4>{product.tag}</h4>
-              <p>{product.price} Bs</p>
-            </div>
+              <p>{product.name}</p>
+              <i className={styles.price}>Bs {product.price}</i>
+            </Link>
           )
+          }
+        </section>
       }
     </div>
   );
